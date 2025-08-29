d0b3ceb0dcfeb77317c7441300a9f1b4fb4776aa
Dongjun Kim <dg047609@gmail.com>
2025-08-30 01:39:47 +0900

    FAC implementation, CRNN without BiGRU

--- model.py
+++ model.py
@@ -1,14 +1,12 @@
-#Some codes are adopted from https://github.com/DCASE-REPO/DESED_task
-#model name == Frequency Dynamic Convolution CRNN
-
-#Author : Dongjun Kim, Donghyeon Lee
-
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
+import torch
 import yaml
 
+"""
+model name : FAC(Frequency Aware Convolution)
+Author : Dongjun Kim, DongHyeon Lee
+
+"""
 class GLU(nn.Module):
     def __init__(self, in_dim):
         super(GLU, self).__init__()
@@ -37,185 +35,127 @@ class ContextGating(nn.Module):
         res = x * sig
         return res
 
-
-class Dynamic_conv2d(nn.Module):
-    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, bias=False, n_basis_kernels=4,
-                 temperature=31, pool_dim='freq'):
-        super(Dynamic_conv2d, self).__init__()
-
-        self.in_planes = in_planes
-        self.out_planes = out_planes
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
-        self.pool_dim = pool_dim
-
-        self.n_basis_kernels = n_basis_kernels
-        self.attention = attention2d(in_planes, self.kernel_size, self.stride, self.padding, n_basis_kernels,
-                                     temperature, pool_dim)
-
-        self.weight = nn.Parameter(torch.randn(n_basis_kernels, out_planes, in_planes, self.kernel_size, self.kernel_size),
-                                   requires_grad=True)
-
-        if bias:
-            self.bias = nn.Parameter(torch.Tensor(n_basis_kernels, out_planes))
-        else:
-            self.bias = None
-
-        for i in range(self.n_basis_kernels):
-            nn.init.kaiming_normal_(self.weight[i])
-
-    def forward(self, x): #x size : [bs, in_chan, frames, freqs]
-        if self.pool_dim in ['freq', 'chan']:
-            softmax_attention = self.attention(x).unsqueeze(2).unsqueeze(4)    # size : [bs, n_ker, 1, frames, 1]
-        elif self.pool_dim == 'time':
-            softmax_attention = self.attention(x).unsqueeze(2).unsqueeze(3)    # size : [bs, n_ker, 1, 1, freqs]
-        elif self.pool_dim == 'both':
-            softmax_attention = self.attention(x).unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)    # size : [bs, n_ker, 1, 1, 1]
-
-        batch_size = x.size(0)
-
-        aggregate_weight = self.weight.view(-1, self.in_planes, self.kernel_size, self.kernel_size) # size : [n_ker * out_chan, in_chan]
-
-        if self.bias is not None:
-            aggregate_bias = self.bias.view(-1)
-            output = F.conv2d(x, weight=aggregate_weight, bias=aggregate_bias, stride=self.stride, padding=self.padding)
-        else:
-            output = F.conv2d(x, weight=aggregate_weight, bias=None, stride=self.stride, padding=self.padding)
-            # output size : [bs, n_ker * out_chan, frames, freqs]
-
-        output = output.view(batch_size, self.n_basis_kernels, self.out_planes, output.size(-2), output.size(-1))
-        # output size : [bs, n_ker, out_chan, frames, freqs]
-
-        if self.pool_dim in ['freq', 'chan']:
-            assert softmax_attention.shape[-2] == output.shape[-2]
-        elif self.pool_dim == 'time':
-            assert softmax_attention.shape[-1] == output.shape[-1]
-
-        output = torch.sum(output * softmax_attention, dim=1)  # output size : [bs, out_chan, frames, freqs]
-
-        return output
+class Positional_Encoding(nn.Module):
+    def __init__(self, freq_bins):
+        super().__init__()
+        self.freq_bins = freq_bins
+        freq = torch.arange(self.freq_bins)
+        pos_freq = torch.sin((torch.pi/2) * (freq / self.freq_bins))
+        self.register_buffer('pos_freq', pos_freq.view(1,1,-1,1))
+    def forward(self, x):
+        return self.pos_freq
 
 
-class attention2d(nn.Module):
-    def __init__(self, in_planes, kernel_size, stride, padding, n_basis_kernels, temperature, pool_dim):
-        super(attention2d, self).__init__()
-        self.pool_dim = pool_dim
-        self.temperature = temperature
+class self_attention(nn.Module):
+    def __init__(self, freq_bins):
+        super().__init__()
+        self.fcn = nn.Linear(in_features=freq_bins, out_features=1)
+        self.activ = nn.Sigmoid()
+    def forward(self, x):
+        x = torch.mean(x,dim=2)
+        x = self.fcn(x)
+        x = self.activ(x)
+        x = x.squeeze(-1)
+        return x
 
-        hidden_planes = int(in_planes / 4)
-
-        if hidden_planes < 4:
-            hidden_planes = 4
-
-        if not pool_dim == 'both':
-            self.conv1d1 = nn.Conv1d(in_planes, hidden_planes, kernel_size, stride=stride, padding=padding, bias=False)
-            self.bn = nn.BatchNorm1d(hidden_planes)
-            self.relu = nn.ReLU(inplace=True)
-            self.conv1d2 = nn.Conv1d(hidden_planes, n_basis_kernels, 1, bias=True)
-            for m in self.modules():
-                if isinstance(m, nn.Conv1d):
-                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                    if m.bias is not None:
-                        nn.init.constant_(m.bias, 0)
-                if isinstance(m, nn.BatchNorm1d):
-                    nn.init.constant_(m.weight, 1)
-                    nn.init.constant_(m.bias, 0)
-        else:
-            self.fc1 = nn.Linear(in_planes, hidden_planes)
-            self.relu = nn.ReLU(inplace=True)
-            self.fc2 = nn.Linear(hidden_planes, n_basis_kernels)
-
-    def forward(self, x): #x size : [bs, chan, frames, freqs]
-        if self.pool_dim == 'freq':
-            x = torch.mean(x, dim=3)  #x size : [bs, chan, frames]
-        elif self.pool_dim == 'time':
-            x = torch.mean(x, dim=2)  #x size : [bs, chan, freqs]
-        elif self.pool_dim == 'both':
-            # x = torch.mean(torch.mean(x, dim=2), dim=1)  #x size : [bs, chan]
-            x = F.adaptive_avg_pool2d(x, (1, 1)).squeeze(-1).squeeze(-1)
-        elif self.pool_dim == 'chan':
-            x = torch.mean(x, dim=1)  #x size : [bs, freqs, frames]
-
-        if not self.pool_dim == 'both':
-            x = self.conv1d1(x)               #x size : [bs, hid_chan, frames]
-            x = self.bn(x)
-            x = self.relu(x)
-            x = self.conv1d2(x)               #x size : [bs, n_ker, frames]
-        else:
-            x = self.fc1(x)               #x size : [bs, hid_chan]
-            x = self.relu(x)
-            x = self.fc2(x)               #x size : [bs, n_ker]
-
-        return F.softmax(x / self.temperature, 1)
-
+class FAC(nn.Module):
+    def __init__(self,freq_bins):
+        super().__init__()
+        self.attn = self_attention(freq_bins=freq_bins)
+        self.pos = Positional_Encoding(freq_bins=freq_bins)
+    def forward(self, x):
+        attn_x = self.attn(x)
+        attn_x = attn_x.view(attn_x.size(0), attn_x.size(1), 1, 1)
+        pos_x = self.pos(x)
+        x = (attn_x + pos_x).transpose(2,3) + x
+        return x
 
 class CNN(nn.Module):
-    def __init__(self,
-                 n_input_ch,
-                 activation="Relu",
-                 conv_dropout=0,
-                 kernel=[3, 3, 3],
-                 pad=[1, 1, 1],
-                 stride=[1, 1, 1],
-                 n_filt=[64, 64, 64],
-                 pooling=[(1, 4), (1, 4), (1, 4)],
-                 normalization="batch",
-                 n_basis_kernels=4,
-                 DY_layers=[0, 1, 1, 1, 1, 1, 1],
-                 temperature=31,
-                 pool_dim='freq'):
+    def __init__(
+        self,
+        n_in_channel,
+        activation="cg",
+        conv_dropout=0,
+        kernel_size=[3, 3, 3],
+        padding=[1, 1, 1],
+        stride=[1, 1, 1],
+        nb_filters=[64, 64, 64],
+        pooling=[(1, 4), (1, 4), (1, 4)],
+        normalization="batch",
+        fac_layers=[1, 1, 1, 1, 1, 1, 1], 
+        freq_bins=[128,64,32,16,8,4,2,1],
+        **transformer_kwargs
+    ):
+        """
+            Initialization of CNN network s
+
+        Args:
+            n_in_channel: int, number of input channel
+            activation: str, activation function
+            conv_dropout: float, dropout
+            kernel_size: kernel size
+            padding: padding
+            stride: list, stride
+            nb_filters: number of filters
+            pooling: list of tuples, time and frequency pooling
+            normalization: choose between "batch" for BatchNormalization and "layer" for LayerNormalization.
+        """
         super(CNN, self).__init__()
-        self.n_filt = n_filt
-        self.n_filt_last = n_filt[-1]
+
+        self.nb_filters = nb_filters
         cnn = nn.Sequential()
 
-        def conv(i, normalization="batch", dropout=None, activ='relu'):
-            in_dim = n_input_ch if i == 0 else n_filt[i - 1]
-            out_dim = n_filt[i]
-            if DY_layers[i] == 1:
-                cnn.add_module("conv{0}".format(i), Dynamic_conv2d(in_dim, out_dim, kernel[i], stride[i], pad[i],
-                                                                   n_basis_kernels=n_basis_kernels,
-                                                                   temperature=temperature, pool_dim=pool_dim))
-            else:
-                cnn.add_module("conv{0}".format(i), nn.Conv2d(in_dim, out_dim, kernel[i], stride[i], pad[i]))
+        def conv(i, normalization="batch", dropout=None, activ="relu"):
+            nIn = n_in_channel if i == 0 else nb_filters[i - 1]
+            nOut = nb_filters[i]
+
+            if fac_layers[i]==1:
+                cnn.add_module(f"fac_conv{i}", FAC(freq_bins=freq_bins[i])),
+                cnn.add_module(f"conv{i}", 
+                nn.Conv2d(nIn, nOut, kernel_size[i], stride[i], padding[i]),
+                )
             if normalization == "batch":
-                cnn.add_module("batchnorm{0}".format(i), nn.BatchNorm2d(out_dim, eps=0.001, momentum=0.99))
+                cnn.add_module(
+                    "batchnorm{0}".format(i),
+                    nn.BatchNorm2d(nOut, eps=0.001, momentum=0.99),
+                )
             elif normalization == "layer":
-                cnn.add_module("layernorm{0}".format(i), nn.GroupNorm(1, out_dim))
+                cnn.add_module("layernorm{0}".format(i), nn.GroupNorm(1, nOut))
 
             if activ.lower() == "leakyrelu":
-                cnn.add_module("Relu{0}".format(i), nn.LeakyReLu(0.2))
+                cnn.add_module("relu{0}".format(i), nn.LeakyReLU(0.2))
             elif activ.lower() == "relu":
-                cnn.add_module("Relu{0}".format(i), nn.ReLu())
+                cnn.add_module("relu{0}".format(i), nn.ReLU())
             elif activ.lower() == "glu":
-                cnn.add_module("glu{0}".format(i), GLU(out_dim))
+                cnn.add_module("glu{0}".format(i), GLU(nOut))
             elif activ.lower() == "cg":
-                cnn.add_module("cg{0}".format(i), ContextGating(out_dim))
+                cnn.add_module("cg{0}".format(i), ContextGating(nOut))
 
             if dropout is not None:
                 cnn.add_module("dropout{0}".format(i), nn.Dropout(dropout))
 
-        for i in range(len(n_filt)):
+        # 128x862x64
+        for i in range(len(nb_filters)):
             conv(i, normalization=normalization, dropout=conv_dropout, activ=activation)
-            cnn.add_module("pooling{0}".format(i), nn.AvgPool2d(pooling[i]))
+            cnn.add_module(
+                "pooling{0}".format(i), nn.AvgPool2d(pooling[i])
+            )  # bs x tframe x mels
+
         self.cnn = cnn
 
-    def forward(self, x):    #x size : [bs, chan, frames, freqs]
+    def forward(self, x):
+        """
+        Forward step of the CNN module
+
+        Args:
+            x (Tensor): input batch of size (batch_size, n_channels, n_frames, n_freq)
+
+        Returns:
+            Tensor: batch embedded
+        """
+        # conv features
         x = self.cnn(x)
         return x
-
-
-class BiGRU(nn.Module):
-    def __init__(self, n_in, n_hidden, dropout=0, num_layers=1):
-        super(BiGRU, self).__init__()
-        self.rnn = nn.GRU(n_in, n_hidden, bidirectional=True, dropout=dropout, batch_first=True, num_layers=num_layers)
-
-    def forward(self, x):
-        #self.rnn.flatten_parameters()
-        x, _ = self.rnn(x)
-        return x
-
 
 class CRNN(nn.Module):
     def __init__(self,
@@ -234,7 +174,6 @@ class CRNN(nn.Module):
         self.n_class = n_class
 
         self.cnn = CNN(n_input_ch=n_input_ch, activation=activation, conv_dropout=conv_dropout, **convkwargs)
-        self.rnn = BiGRU(n_in=self.cnn.n_filt[-1], n_hidden=n_RNN_cell, dropout=rec_dropout, num_layers=n_RNN_layer)
 
         self.dropout = nn.Dropout(conv_dropout)
         self.sigmoid = nn.Sigmoid()
@@ -266,6 +205,7 @@ class CRNN(nn.Module):
         #rnn
         # x = self.rnn(x) #x size : [bs, frames, 2 * chan]
         x = self.dropout(x)
+
         #classifier
         strong = self.dense(x) #strong size : [bs, frames, n_class]
         strong = self.sigmoid(strong)
@@ -278,11 +218,3 @@ class CRNN(nn.Module):
             weak = strong.mean(1)
 
         return weak
-
-if __name__ == "__main__":
-    path = '/home/baek/Desktop/Deepship/default.yaml'
-    with open(path, "r") as f:
-        configs = yaml.safe_load(f)
-    input = torch.randn([48,128,626])
-    net = CRNN(**configs["CRNN"])
-    output = net(input)
